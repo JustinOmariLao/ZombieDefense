1c7f128be84b1ca9131cfaf70fd735ba1aad69e0
jlao3 <j.chancelao@gmail.com>
2021-12-01 16:27:46 -0800

    Game scene implemented and Camera Animation

--- ZombieDefense/Assets/Scripts/Game_Script.cs
+++ ZombieDefense/Assets/Scripts/Game_Script.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Wave_Script : MonoBehaviour
+public class Game_Script : MonoBehaviour
 {
+    public Animator cameraAnimator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraAnimator.Play("Camera_Down");
     }
 
     // Update is called once per frame
