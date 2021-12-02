7c2bbfd24dbb1c798285b5883678c980ec60833d
jlao3 <j.chancelao@gmail.com>
2021-12-01 20:34:57 -0800

    Zombie Prefab, Character Model, Camera Adjustments

--- ZombieDefense/Assets/Scripts/Game_Script.cs
+++ ZombieDefense/Assets/Scripts/Game_Script.cs
@@ -5,17 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class Game_Script : MonoBehaviour
 {
-    public Animator cameraAnimator;
+    public GameObject mainCam;
+    public GameObject player;
     
+    private float beginWave = 5f;
+
+    private bool waveStart = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        cameraAnimator.Play("Camera_Down");
+        var camAnim = mainCam.GetComponent<Animator>();
+        camAnim.Play("Camera_Down");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!waveStart) {
+            beginWave -= Time.deltaTime;
+            if (beginWave < 0) {
+                waveStart = true;
+            }
+        }
+        if (waveStart == true) {
+            mainCam.transform.SetParent(player.transform);
+        }
+
     }
 }
